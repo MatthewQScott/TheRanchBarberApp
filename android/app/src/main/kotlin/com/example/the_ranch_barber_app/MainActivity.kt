930150082f76e26cd10f5f8c4f9274c111857fc0
MQS-CPP <mqscott@cpp.edu>
2023-06-16 08:56:11 -0700

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/the_ranch_barber_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.the_ranch_barber_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+
+void main() {
+  runApp(
+    const MaterialApp(
+      home: WebViewApp(),
+    ),
+  );
+}
+
+class WebViewApp extends StatefulWidget {
+  const WebViewApp({super.key});
+
+  @override
+  State<WebViewApp> createState() => _WebViewAppState();
+}
+
+class _WebViewAppState extends State<WebViewApp> {
+  late final WebViewController controller;
+
+
+  @override
+  void initState() {
+    super.initState();
+    controller = WebViewController()
+    ..setJavaScriptMode(JavaScriptMode.unrestricted)
+      ..setNavigationDelegate(
+        NavigationDelegate(
+        onProgress: (int progress) {
+
+        },
+        onPageStarted: (String url) {},
+        onPageFinished: (String url) {},
+        onWebResourceError: (WebResourceError error) {
+
+        },
+          onNavigationRequest: (NavigationRequest request) {
+            if (request.url.startsWith('tel:')) {
+              return NavigationDecision.prevent;
+            }
+            return NavigationDecision.navigate;
+          },
+        ),
+      )
+      ..loadRequest(
+        Uri.parse('https://the-ranch-barber-co.square.site/'),
+      );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('The Ranch Barber (WebView)'),
+      ),
+      body:
+      WebViewWidget(
+        controller: controller,
+      ),
+    );
+  }
+}
